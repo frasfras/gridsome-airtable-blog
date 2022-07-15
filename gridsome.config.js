6973582e16828a6aecb6fd890d6158f79556e34a
frasfras <65541080+frasfras@users.noreply.github.com>
2022-07-15 14:17:35 +0800

    Add files via upload

---
+++ gridsome.config.js
@@ -0,0 +1,40 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: "@gridsome/source-airtable",
+      options: {
+        // Add these to a .env file
+        // Details on finding these values can be found at:
+        // https://gridsome.org/plugins/@gridsome/source-airtable
+        apiKey: process.env.AIRTABLE_KEY, //required
+        baseId: process.env.AIRTABLE_BASE, //required
+        tables: [
+          {
+            name: "Events",
+            typeName: "Event", //required - needs to match template name
+            select: {}, //optional
+            links: [], //optional
+          },
+          // Comment in this section and the line in `templates` for multiple tables!
+          // {
+          //   name: "Parties",
+          //   typeName: "Parties", //required - needs to match template name
+          //   select: {}, //optional
+          //   links: [], //optional
+          // },
+        ],
+      },
+    },
+  ],
+  templates: {
+    Event: "/events/:id", //optional
+    // Parties: "/parties/:id", //optional
+  },
+}
